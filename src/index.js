117d07d1ea1549f24aaa3b43be684b3a1858dbfb
SergeyLebidko <it.sergeyler@mail.ru>
2021-06-26 19:12:06 +0300

    Добавлен фон

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import './index.css';
+import back from './images/back.png';
+
+document.getElementsByTagName('body')[0].style.backgroundImage = `url(${back})`;
 
 ReactDOM.render(
     <App/>,
