33617fb67255d9bd42783ddbfe4fb0359d2fde14
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-07 11:41:25 +0300

    В Display.js добавлена автопрокрутка

--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import style from './Display.module.css';
 
-function Display({data}){
+function Display({data}) {
+    let container = useRef(null);
+
+    useEffect(() => {
+        let {clientHeight, scrollHeight} = container.current;
+        if (clientHeight < scrollHeight) container.current.scrollTop = scrollHeight;
+    }, [data]);
+
     return (
-        <ul className={style.container}>
+        <ul className={style.container} ref={container}>
             {data.map((item, index) => <li key={index}>{item}</li>)}
         </ul>
     )
