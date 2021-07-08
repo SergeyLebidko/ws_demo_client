de339e4c2651f0ed5584b5ce445d5cc4692dcc70
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-08 19:34:16 +0300

    Завершен код имитатора чата

--- src/GroupEchoContainer/GroupEchoContainer.js
+++ src/GroupEchoContainer/GroupEchoContainer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import GroupEchoElement from '../GroupEchoElement/GroupEchoElement';
 import style from './GroupEchoContainer.module.css';
+import commonStyle from '../common.module.css';
 
 function GroupEchoContainer() {
     return (
         <div className={style.container}>
-            <GroupEchoElement/>
-            <GroupEchoElement/>
-            <GroupEchoElement/>
+            <h1 className={commonStyle.header}>
+                Модель чата. Каждый из трех клиентов подключается по протоколу webscoket к серверу и отправляет
+                сообщения.
+                Отправленные сообщения тут же поступают к остальным клиентам.
+            </h1>
+            <GroupEchoElement setUpData={['Красный', 'Синий', 'Зеленый', 'Белый']}/>
+            <GroupEchoElement setUpData={['Огонь', 'Вода', 'Воздух', 'Земля']}/>
+            <GroupEchoElement setUpData={['Python', 'JavaScript', 'PHP', 'C#']}/>
         </div>
     )
 }
--- src/GroupEchoElement/GroupEchoElement.js
+++ src/GroupEchoElement/GroupEchoElement.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Display from '../Display/Display';
+import {HOST} from '../settings';
 import style from './GroupEchoElement.module.css';
 import commonStyle from '../common.module.css';
 
-function GroupEchoElement() {
+function GroupEchoElement({setUpData}) {
+    let [data, setData] = useState([]);
+    let connection = useRef(null);
+
+    let show = (text) => {
+        setData(oldData => [...oldData, text]);
+    }
+
+    useEffect(() => {
+        let socket = new WebSocket(HOST + 'ws/group_echo/');
+        socket.onopen = () => {
+            show('Соединение установлено');
+            connection.current = socket;
+        };
+        socket.onmessage = receiveHandler;
+        socket.onclose = () => show('Соединение разорвано');
+
+        return () => socket.close();
+    }, []);
+
+    // Обработчик приема сообщений из сокета
+    let receiveHandler = event => {
+        show(event.data);
+    }
+
+    // Обработчик клика по кнопке отправки
+    let changeMessageHandler = () => {
+        if (!connection.current) return;
+        let value = setUpData[Math.floor(Math.random() * setUpData.length)];
+        connection.current.send(value);
+    }
+
     return (
         <div className={style.container}>
-            <Display data={[]}/>
+            <Display data={data}/>
             <div className={commonStyle.control}>
-                <input type="button" className={commonStyle.button + ' ' + commonStyle.enabled} value="Отправить"/>
+                <input
+                    type="button"
+                    className={commonStyle.button + ' ' + commonStyle.enabled} value="Отправить"
+                    onClick={changeMessageHandler}
+                />
             </div>
         </div>
     );
