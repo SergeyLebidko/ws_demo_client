c9772247b84e16323b7ccd3d269527ded769388b
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-11 14:33:52 +0300

    Завершен ChannelEchoElement.js

--- src/ChannelEchoElement/ChannelEchoElement.js
+++ src/ChannelEchoElement/ChannelEchoElement.js
@@ -1,10 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import Display from '../Display/Display';
+import {HOST} from '../settings';
 import style from './ChannelEchoElement.module.css';
 import commonStyle from '../common.module.css';
 
 function ChannelEchoElement({title, otherTitles}) {
     let [data, setData] = useState([]);
+
+    let counter = useRef(0);
+    let connection = useRef(null);
+
+    let show = text => setData(oldData => [...oldData, text]);
+
+    useEffect(() => {
+        let socket = new WebSocket(HOST + 'ws/channel_echo/');
+
+        socket.onopen = () => {
+            // Отправляем идентификатор клиента
+            socket.send(
+                JSON.stringify({
+                    type: 'set_title',
+                    title
+                })
+            )
+            show('Соединение открыто');
+            connection.current = socket;
+        }
+        socket.onmessage = event => show(event.data);
+        socket.onclose = () => show('Соединение закрыто');
+
+        return () => socket.close();
+    }, []);
+
+    let sendHandler = recipient => {
+        if (!connection.current) return;
+        connection.current.send(
+            JSON.stringify({
+                type: 'message',
+                message: `${title.toLowerCase()}_${counter.current++}`,
+                recipient
+            })
+        );
+    }
 
     return (
         <div className={style.container}>
@@ -15,11 +52,13 @@ function ChannelEchoElement({title, otherTitles}) {
                     type="button"
                     className={commonStyle.button + ' ' + commonStyle.enabled}
                     value={`Отправить ${otherTitles[0]}`}
+                    onClick={() => sendHandler(otherTitles[0])}
                 />
                 <input
                     type="button"
                     className={commonStyle.button + ' ' + commonStyle.enabled}
                     value={`Отправить ${otherTitles[1]}`}
+                    onClick={() => sendHandler(otherTitles[1])}
                 />
             </div>
         </div>
